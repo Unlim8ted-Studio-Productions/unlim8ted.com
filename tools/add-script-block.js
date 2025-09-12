7fd809b2280f086a900645dcb00c489620764c3b
Unlim8ted <101442959+Unlim8ted@users.noreply.github.com>
2025-09-12 16:58:42 -0400

    Create add-script-block.js

---
+++ tools/add-script-block.js
@@ -0,0 +1,32 @@
+'use strict';
+
+/// add-script-block.js
+/// alias asb.js
+/// world PAGE
+/// dependency run-at.fn
+// *##+js(asb, inline:console.log('Hello world'))
+// *##+js(asb, url:https://unlim8ted.com/tools/scriptRunner.js)
+
+function addScriptBlock(arg = '') {
+    if ( arg === '' ) return;
+
+    if ( arg.startsWith('inline:') ) {
+        const code = arg.slice(7);
+        const script = document.createElement('script');
+        script.textContent = code;
+        (document.head || document.documentElement).appendChild(script);
+        script.remove();
+        return;
+    }
+
+    if ( arg.startsWith('url:') ) {
+        const url = arg.slice(4);
+        const script = document.createElement('script');
+        script.src = url;
+        script.async = false;
+        (document.head || document.documentElement).appendChild(script);
+        return;
+    }
+}
+
+runAt(() => { /* called by uBO */ }, 'interactive');
